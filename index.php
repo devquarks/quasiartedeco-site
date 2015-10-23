0bd8c27242aaa1bae1efbf61df66f42927bb8b0a
Juan Pablo Norverto <juan.norverto@gmail.com>
2015-10-23 03:19:21 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Content-Type: text/html; charset=UTF-8');
+include('home.html');
+
+?>;
+
+<!--á => &aacute;-->
+<!--é => &eacute;-->
+<!--í => &iacute;-->
+<!--ó => &oacute;-->
+<!--ú => &uacute;-->
+
